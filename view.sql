3d4b901aaa11bf0dd9a5af3f864cefedc773fa68
joaovieiraantunes <87206306+joaovieiraantunes@users.noreply.github.com>
2022-06-17 19:44:11 +0100

    Create view.sql

---
+++ view.sql
@@ -0,0 +1,15 @@
+create materialized view vendas(ean, cat, ano, trimestre, dia_mes, dia_semana, distrito, concelho, unidades) 
+as 
+select o.ean as ean,
+	o.nome_categoria as cat,
+	extract(year from instante) as ano,
+	extract(quarter from instante) as trimestre,
+	extract(day from instante) as dia_mes,
+	extract(dow from instante) as dia_semana,
+	r.distrito as distrito,
+	r.concelho as concelho,
+	e.unidades as unidades
+from produto o 
+	join evento_reposicao e on o.ean=e.ean
+	join instalada_em i on e.num_serie=i.num_serie and e.fabricante=i.fabricante
+	join ponto_de_retalho r on i.ivm_local=r.nome_retalho;
