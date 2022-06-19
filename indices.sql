86bd5858763ebe7b467e761683c619ed15cb0cf8
JoseCruz2002 <jpcruz2002@gmail.com>
2022-06-19 14:36:36 +0100

    corrected the first index???

--- indices.sql
+++ indices.sql
@@ -1,25 +1,30 @@
 -- Indices --
 
 -- 1 --
-select distinct R.nome
+select distinct R.nome_retalhista
 from retalhista R, responsavel_por P
-where R.tin = P.tin and P. nome_cat = 'Frutos'
+where R.tin = P.tin and P.nome_categoria = 'CATEGORIA_2'
 
-alter index retalhista_tin_index on retalhista hash (tin);
-create index responsavelpor_tin_index on responsavel_por hash (tin, nome_categoria);
+alter index retalhista_tin_index on retalhista using hash (tin);
+create index if not exists respor_cat_index on responsavel_por using hash (tin);
 
 -- Dado que esta query implica filtar os valores da tabela retalhista por
 -- uma igualdade com um outro valor, modifica-se o index dessa tabela no 
 -- tin para um do tipo hash pois este é o melhor para seleções por igualdade
 -- (modifica-se e não cria-se pois o index já existe, dado que tin é chave 
 -- primária da relação retalhista).
--- Os outros dois indexes foram criados pela mesma razão, com o tipo hash
--- pois vão ser usados para seleção. 
+-- Criou-se também um índice para o atributo nome_categoria da tabela 
+-- responsavel_por para acelerar a procura por entradas da tabela com a categoria
+-- escolhida, dado que e uma igualdade usa-se hash. No nosso caso a seletividade
+-- da procura do nome_categoria é igual comparando a do tin na reponsavel_por logo 
+-- escolhemos o tin por ser foreign key vinda da tabela retalhista. Se as 
+-- seletividades fossem diferentes criaria-mos um indice para o atributo com 
+-- maior seletividade.
 
 
 -- 2 --
-SELECT T.nome, count(T.ean)
+SELECT T.nome_categoria, count(T.ean)
 FROM produto P, tem_categoria T
-WHERE p.cat = T.nome and P.desc like 'A%'
-GROUP BY T.nome
+WHERE p.nome_categoria = T.nome_categoria and P.descricao like 'D%'
+GROUP BY T.nome_categoria
 
