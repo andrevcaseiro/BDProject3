e16bf22765a46cd5b5734b61e0b83c10712c1e6a
JoseCruz2002 <jpcruz2002@gmail.com>
2022-06-19 19:02:21 +0100

    add the indexes for the second part of ex7

--- indices.sql
+++ indices.sql
@@ -3,7 +3,7 @@
 -- 1 --
 select distinct R.nome_retalhista
 from retalhista R, responsavel_por P
-where R.tin = P.tin and P.nome_categoria = 'CATEGORIA_2'
+where R.tin = P.tin and P.nome_categoria = 'CATEGORIA_5'
 
 alter index retalhista_tin_index on retalhista using hash (tin);
 create index if not exists respor_cat_index on responsavel_por using hash (tin);
@@ -12,19 +12,38 @@ create index if not exists respor_cat_index on responsavel_por using hash (tin);
 -- uma igualdade com um outro valor, modifica-se o index dessa tabela no 
 -- tin para um do tipo hash pois este é o melhor para seleções por igualdade
 -- (modifica-se e não cria-se pois o index já existe, dado que tin é chave 
--- primária da relação retalhista).
+-- primária da tabela retalhista).
 -- Criou-se também um índice para o atributo nome_categoria da tabela 
 -- responsavel_por para acelerar a procura por entradas da tabela com a categoria
 -- escolhida, dado que e uma igualdade usa-se hash. No nosso caso a seletividade
--- da procura do nome_categoria é igual comparando a do tin na reponsavel_por logo 
--- escolhemos o tin por ser foreign key vinda da tabela retalhista. Se as 
--- seletividades fossem diferentes criaria-mos um indice para o atributo com 
--- maior seletividade.
+-- da procura do nome_categoria é maior que a do tin daí criarmos um indice para
+-- esse atributo.
 
 
 -- 2 --
 SELECT T.nome_categoria, count(T.ean)
 FROM produto P, tem_categoria T
-WHERE p.nome_categoria = T.nome_categoria and P.descricao like 'D%'
+WHERE P.nome_categoria = T.nome_categoria and P.descricao like 'D%'
 GROUP BY T.nome_categoria
 
+create index if not exists tem_cat_index on tem_categoria (nome_categoria);
+create index if not exists prod_descr_index on produto (descricao);
+
+-- Índices que se podia pensar criar:
+--   - um de tipo hash na tabela produto para o nome_categoria;
+--   - um de tipo hash ou btree na tabela tem_categoria para o nome_categoria;
+--   - um de tipo btree na tabela produto para a descricao.
+
+--------------------------- analise de seletividade para o 2 --------------------
+SELECT count(t.nome_categoria)
+FROM produto P, tem_categoria T
+WHERE P.nome_categoria = T.nome_categoria 
+-- resultado = 668
+
+SELECT count(t.nome_categoria)
+FROM produto P, tem_categoria T
+WHERE P.descricao = 'DESCRICAO_PRODUTO_1'
+-- resultado = 546
+----------------------------------------------------------------------------------
+
+-- btree para o nome_categoria da tem_categoria e btree para a descricao do produto????
